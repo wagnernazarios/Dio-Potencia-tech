bd8d96b3a5d34a66233003a847d3c74b122192ce
wagnernazarios <wagnernazario124@gmail.com>
2023-04-29 22:03:26 -0300

    finish the function class

--- 04 - funcoes/aula2.js
+++ 04 - funcoes/aula2.js
@@ -2,6 +2,7 @@ function escrevaMeuNome(nome) {
   return "Meu nome é: " + nome;
 }
 
+//Maior ou menor de idade
 function classificarIdade(idade) {
   if (idade < 18) {
     return "Menor de idade!";
@@ -13,17 +14,39 @@ function classificarIdade(idade) {
 // console.log(escrevaMeuNome("Wagner, e sou ") + classificarIdade(22));
 
 //---------------------------------------------------
+// Pagamento
 
-function metodoDePagamento(preco, formaDePagamento) {
-  if (formaDePagamento === 1) {
-    return preco - preco * 0.1;
-  } else if (formaDePagamento === 2) {
-    return preco - preco * 0.15;
-  } else if (formaDePagamento === 3) {
-    return preco;
-  } else {
-    return preco + preco * 0.15;
-  }
+function aplicarDesconto(valor, desconto) {
+  return valor - valor * (desconto / 100);
 }
 
-console.log(metodoDePagamento(200, 4));
+function aplicarJuros(valor, juros) {
+  return valor + valor * (juros / 100);
+}
+
+const valorDeEtiqueta = 100;
+const formaDePagamento = 4;
+
+if (formaDePagamento === 1) {
+  console.log(aplicarDesconto(valorDeEtiqueta, 10));
+} else if (formaDePagamento === 2) {
+  console.log(aplicarDesconto(valorDeEtiqueta, 15));
+} else if (formaDePagamento === 3) {
+  console.log(valorDeEtiqueta);
+} else {
+  console.log(aplicarJuros(valorDeEtiqueta, 10));
+}
+
+// function metodoDePagamento(preco, formaDePagamento) {
+//   if (formaDePagamento === 1) {
+//     return preco - preco * 0.1;
+//   } else if (formaDePagamento === 2) {
+//     return preco - preco * 0.15;
+//   } else if (formaDePagamento === 3) {
+//     return preco;
+//   } else {
+//     return preco + preco * 0.15;
+//   }
+// }
+
+// console.log(metodoDePagamento(200, 2));
