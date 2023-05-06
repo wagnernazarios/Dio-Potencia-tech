b21de53d3bac6364d1747d96b8cb27632628cb47
wagnernazarios <wagnernazario124@gmail.com>
2023-05-06 16:57:43 -0300

    learning debugging the looping

---
+++ 06 - arrays e estrutura de repeticoes/aula.js
@@ -0,0 +1,20 @@
+// const notas = [];
+
+// notas.push(5);
+// notas.push(4);
+// notas.push(7);
+// notas.push(2);
+// notas.push(3);
+
+// const soma = notas[0] + notas[1] + notas[2] + notas[3] + notas[4];
+
+// console.log(soma / notas.length);
+
+const numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let i = 0; i < numeros.length; i++) {
+  const numero = numeros[i];
+  if (numero % 2 == 0) {
+    console.log(numero);
+  }
+}
