d88eb470577fa56c1c17f1a1a112287caa90d1c8
wagnernazarios <wagnernazario124@gmail.com>
2023-05-06 10:11:38 -0300

    exercicies 3 done!

--- 05 - objetos/exercicio3.js
+++ 05 - objetos/exercicio3.js
@@ -12,11 +12,29 @@ class Pessoa {
   calcularImc() {
     return this.peso / (this.altura * this.altura);
   }
-}
-const jose = new Pessoa("Jose", 70, 1.75);
 
-console.log(jose.calcularImc());
+  classificarIMC() {
+    const imc = this.calcularImc;
+
+    if (imc < 18.5) {
+      return "Abaixo do peso";
+    } else if (imc >= 18.5 && imc <= 25) {
+      return "Peso normal!";
+    } else if (imc > 25 && imc <= 30) {
+      return "Acima do peso!";
+    } else if (imc > 30 && imc <= 40) {
+      return "Obeso!";
+    } else {
+      return "Obesidade grave!";
+    }
+  }
+}
+
+// const jose = new Pessoa("Jose", 70, 1.75);
+
+// console.log(jose.calcularImc());
 
 const wagner = new Pessoa("Wagner", 100, 1.82);
 
-console.log(wagner.calcularImc());
+console.log(wagner);
+console.log(wagner.calcularImc() + "\n" + wagner.classificarIMC());
