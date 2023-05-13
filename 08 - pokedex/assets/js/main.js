64eae32a7f824fb741290276c878621b39218086
wagnernazarios <wagnernazario124@gmail.com>
2023-05-13 14:33:38 -0300

    deixando o codigo mais limpo com arrow function

--- 08 - pokedex/assets/js/main.js
+++ 08 - pokedex/assets/js/main.js
@@ -2,10 +2,8 @@ const offset = 0;
 const limit = 10;
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
-fetch(url).then(function (response) {
-  console.log(response);
-});
-
-const x = 10 + 20;
-
-console.log(x);
+fetch(url)
+  .then((response) => response.json())
+  .then((jsonBody) => console.log(jsonBody))
+  .catch((error) => console.log(error))
+  .finally(() => console.log("Requisição completa!"));
